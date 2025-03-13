09c033e26967838aa6db987ac8aba9ce9a164d6c
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-03-13 11:50:58 +0700

    menambahkan prosedur input

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int arr[20];    //Deklarasi variable global array dengan panjang 20 dan nama arr
+int n;  //Deklarasi variable global n untuk menyimpan elemen pada array
+
+void input(){   //Proseur input
+    int d;
+    while (true){
+        cout << "masukkan banyaknya elemen pada array";
+        cin >> n;
+        if  (n <=20){
+            break;
+        }
+        else {
+            cout << "\nArray maksimal 20 elemen. \n";
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i=0;i<n; i++){
+        cout << "Data ke-" << (i+1) << " = ";
+        cin >> arr[i];
+    }
+}
+
+
+int main() {
+
+}
