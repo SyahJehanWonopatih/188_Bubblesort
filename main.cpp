aadd595909fc68cdfe07874e52ac56ec09dc3f8f
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-03-13 11:52:07 +0700

    menambahkan prosedur bubblesort array

--- main.cpp
+++ main.cpp
@@ -28,7 +28,23 @@ void input(){   //Proseur input
         cin >> arr[i];
     }
 }
+void bubbleSortArray()
+{
+    int pass = 1;   // step 1
 
+    do {
+        for (int j=0; j<=n-1-pass; j++){   // step 2
+            if (arr[j] > arr[j+1]) {        // step 3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+
+        }
+        pass = pass + 1; // step 4
+    }while(pass <= n-1); // step 5
+}
 
 int main() {
 
